66afdc3fd809fc02b4e057cc8384070093c46efe
yKanon <394769450@qq.com>
2022-01-17 22:14:51 +0800

    feat: basic work

---
+++ webpack-custom-plugin/src/index.js
@@ -0,0 +1,8 @@
+const createElement = () => {
+  const element = document.createElement('div');
+  element.innerHTML = '孔子曰：中午不睡，下午崩溃!孟子曰：孔子说的对!'
+
+  return element;
+}
+
+document.appendChild(createElement());
